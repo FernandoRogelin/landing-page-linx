0dfa7273e9ab115c076137b51d8387d41f914ddc
FernandoRogelin <nando_rogelin@hotmail.com>
2020-10-12 19:59:15 -0300

    fix(css, validate): Organize css file and changed function that validate inputs

--- js/validate.js
+++ js/validate.js
@@ -1,42 +1,45 @@
-function validateEmail(field) {
-  let user = field.value.substring(0, field.value.indexOf("@"));
-  let domain = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-  let btn = document.getElementById('main-share-footer-submit');
-
-  if ((user.length >=1) &&
-      (domain.length >=3) &&
-      (user.search("@")==-1) &&
-      (domain.search("@")==-1) &&
-      (user.search(" ")==-1) &&
-      (domain.search(" ")==-1) &&
-      (domain.search(".")!=-1) &&
-      (domain.indexOf(".") >=1)&&
-      (domain.lastIndexOf(".") < domain.length - 1))
-      isValid('email', true);
-    else
-      isValid('email', false);
-}
-
-function validateName(field) {
-  let name = field.value;
-
-  name >= 1 ? isValid('name', true) : isValid('name', false);
-}
-
-let valid = {
-  email: false,
-  name: false,
-}
-
-function isValid(name, value) {
-  valid[name] = value;
-  let btn = document.getElementById('main-share-footer-submit');
-
-  if (valid.email && valid.name) {
-    btn.classList.remove("disabled");
-    btn.removeAttribute("disabled", "");
-  } else {
-    btn.classList.add("disabled");
-    btn.setAttribute("disabled", "");
+(function() {
+  let valid = {
+    email: false,
+    name: false,
   }
-}
+
+  document.getElementById('labelInput-email').onchange = (e) => {
+    let user = e.target.value.substring(0, e.target.value.indexOf("@"));
+    let domain = e.target.value.substring(e.target.value.indexOf("@")+ 1, e.target.value.length);
+
+    if ((user.length >=1) &&
+        (domain.length >=3) &&
+        (user.search("@")==-1) &&
+        (domain.search("@")==-1) &&
+        (user.search(" ")==-1) &&
+        (domain.search(" ")==-1) &&
+        (domain.search(".")!=-1) &&
+        (domain.indexOf(".") >=1)&&
+        (domain.lastIndexOf(".") < domain.length - 1))
+        isValid('email', true);
+      else
+        isValid('email', false);
+  }
+
+  document.getElementById('labelInput-name').onchange = (e) => {
+    let name = e.target.value;
+
+    name.length >= 1 ? isValid('name', true) : isValid('name', false);
+  }
+
+  function isValid(name, value) {
+    valid[name] = value;
+    let btn = document.getElementById('main-share-footer-submit');
+
+    if (valid.email && valid.name) {
+      btn.classList.remove("disabled");
+      btn.removeAttribute("disabled", "");
+    } else {
+      btn.classList.add("disabled");
+      btn.setAttribute("disabled", "");
+    }
+  }
+})();
+
+
